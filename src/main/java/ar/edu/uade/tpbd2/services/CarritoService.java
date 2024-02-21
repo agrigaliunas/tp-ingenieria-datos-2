a38ffab029dca8fd035479e2dc6625bede35d915
Agustin Grigaliunas <agrigaliunas@ibm.com>
2024-02-21 20:18:41 -0300

    Added repository to service

--- src/main/java/ar/edu/uade/tpbd2/services/CarritoService.java
+++ src/main/java/ar/edu/uade/tpbd2/services/CarritoService.java
@@ -1,12 +1,17 @@
 package ar.edu.uade.tpbd2.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ar.edu.uade.tpbd2.persistence.model.Producto;
+import ar.edu.uade.tpbd2.repositories.CarritoRepository;
 
 @Service
-public class CarritoService {
+public class CarritoService{
 
+
+    @Autowired
+    private CarritoRepository repository;
 
     public void agregarProducto(final Producto producto) {
 
