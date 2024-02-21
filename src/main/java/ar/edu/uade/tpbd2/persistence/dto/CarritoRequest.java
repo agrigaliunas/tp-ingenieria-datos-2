adcc47cd69f3c2b0ae42196acff0013a9fbca8c7
Agustin Grigaliunas <agrigaliunas@ibm.com>
2024-02-21 19:49:54 -0300

    Added producto

--- src/main/java/ar/edu/uade/tpbd2/persistence/dto/CarritoRequest.java
+++ src/main/java/ar/edu/uade/tpbd2/persistence/dto/CarritoRequest.java
@@ -2,6 +2,5 @@ package ar.edu.uade.tpbd2.persistence.dto;
 
 public class CarritoRequest {
 
-
     public
 }
--- src/main/java/ar/edu/uade/tpbd2/persistence/model/Carrito.java
+++ src/main/java/ar/edu/uade/tpbd2/persistence/model/Carrito.java
@@ -1,5 +1,45 @@
 package ar.edu.uade.tpbd2.persistence.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "carrito")
 public class Carrito {
 
+    @Id
+    private String id;
+
+    private String usuarioId;
+
+    private EstadoActual estadoActual;
+
+    private EstadoAnterior estadoAnterior;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getUsuarioId() {
+        return this.usuarioId;
+    }
+
+    public void setUsuarioId(final String usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public Estado getEstadoActual() {
+        return this.estadoActual;
+    }
+
+    public void setEstadoActual(final EstadoActual estadoActual) {
+        this.estadoActual = estadoActual;
+    }
+
+    public Estado getEstadoAnterior() {
+        return this.estadoAnterior;
+    }
+
+    public void setEstadoAnterior(final EstadoAnterior estadoAnterior) {
+        this.estadoAnterior = estadoAnterior;
+    }
 }
