ed3378988105ddeb640ceca7d0de4a1e9205e6ad
Agustin Grigaliunas <agrigaliunas@ibm.com>
2024-02-26 16:21:56 -0300

    Added todo

--- src/main/java/ar/edu/uade/tpbd2/controllers/CatalogoDeProductoController.java
+++ src/main/java/ar/edu/uade/tpbd2/controllers/CatalogoDeProductoController.java
@@ -1,10 +1,18 @@
 package ar.edu.uade.tpbd2.controllers;
 
-import ar.edu.uade.tpbd2.persistence.model.mongo.CatalogoDeProducto;
-import ar.edu.uade.tpbd2.services.CatalogoDeProductosService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ar.edu.uade.tpbd2.persistence.model.mongo.CatalogoDeProducto;
+import ar.edu.uade.tpbd2.services.CatalogoDeProductosService;
 
 @RestController
 @RequestMapping("catalogo_producto")
@@ -14,9 +22,14 @@ public class CatalogoDeProductoController {
     public CatalogoDeProductosService catalogoDeProductosService;
 
     @GetMapping("/{productoID}")
-    public ResponseEntity<CatalogoDeProducto> obtenerPorProducto(@PathVariable("productoID") final String productoID) {
+    public ResponseEntity<CatalogoDeProducto> obtenerPorId(@PathVariable("productoID") final String productoID) {
         return this.catalogoDeProductosService.obtenerCatalogoDeProductoPorID(productoID);
     }
+
+    /*
+     * TODO:
+     * pasar a sql si se crea o actualiza producto
+     */
 
     @PostMapping("")
     public ResponseEntity<CatalogoDeProducto> crearProducto(@RequestBody final CatalogoDeProducto request) {
